727886cf1dda8bc253f35a6de16b676424fdfff0
hliu35 <hliu35@ucsc.edu>
2021-05-10 11:46:22 +0800

    upload latest result

--- query_builder.py
+++ query_builder.py
@@ -7,30 +7,35 @@ from org.apache.lucene.queryparser.classic import MultiFieldQueryParser, QueryPa
 def bq_builder(title, description, analyzer, desc_fields="all"):
 
     # field A
-    parser_A = QueryParser("T", analyzer) #title parser
-    query_raw_A = title
-    query_A = parser_A.parse(query_raw_A)
+    #parser_A = QueryParser("T", analyzer) #title parser
+    #query_raw_A = description
+    #query_A = parser_A.parse(query_raw_A)
 
     # field B
-    parser_B = QueryParser("W", analyzer) #body parser
-    query_raw_B = description
-    query_B = parser_B.parse(query_raw_B)
+    #parser_B = QueryParser("M", analyzer) #body parser
+    #query_raw_B = description
+    #query_B = parser_B.parse(query_raw_B)
 
     # multi-field query C and more
     #query_C = mfQueryParser(['W', 'M'], description, analyzer)
-    #query_C = mfQueryParser(['T', 'W', 'M'], title+" "+description, analyzer)
+    #query_C = mfQueryParser(['T', 'W'], title+" "+description, analyzer)
     query_C = mfQueryParser(['T', 'W', 'M'], description, analyzer)
 
-    query_D = mfQueryParser(["T", "W", "M"], title, analyzer)
+    #query_D = mfQueryParser(["M"], title, analyzer)
 
+    # field E
+    #parser_E = QueryParser("W", analyzer)  # body parser
+    #query_raw_E = description
+    #query_E = parser_B.parse(query_raw_E)
 
 
     # boolean combine
     bquery = BooleanQuery.Builder()
-    #bquery.add(query_A, BooleanClause.Occur.MUST) # SHOULD vs MUST
+    #bquery.add(query_A, BooleanClause.Occur.SHOULD) # SHOULD vs MUST
     #bquery.add(query_B, BooleanClause.Occur.MUST)
     bquery.add(query_C, BooleanClause.Occur.MUST)
     #bquery.add(query_D, BooleanClause.Occur.SHOULD)
+    #bquery.add(query_E, BooleanClause.Occur.SHOULD)
 
     bq_built = bquery.build()
 
